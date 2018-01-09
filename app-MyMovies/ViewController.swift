69437e6fa18eac3c125e40e6471fd505b207cfc1
Walter Junior <juniordeonisio@gmail.com>
2018-01-09 17:51:12 -0200

    First Commit

---
+++ app-MyMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-MyMovies
+//
+//  Created by Walter Junior on 09/01/2018.
+//  Copyright © 2018 Walter Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
